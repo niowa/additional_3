249f71c7fe617492ac990df9f2c3481a8bddb148
niowa <andres7480311@mail.ru>
2017-02-24 15:41:22 +0300

    additional_3/feachure: implement method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function getBase(base) {
-  // your solution
+//module.exports =
+function getBase(base) {
+  let bank = +base;
+  let go = false;
+  for (var i = 2, step = 2; bank != 0;) {
+    if (bank % i === 1) {
+        bank = Math.floor(bank / i);
+        go = true;
+    }
+    else if (go === true) {
+      go = false;
+      bank = +base;
+      step++;
+      i = step;
+    }
+    else i = ++step;
+  }
+  return i;
 }
